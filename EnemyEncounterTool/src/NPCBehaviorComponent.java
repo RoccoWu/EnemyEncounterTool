864213b5eb9040c24d0eaaa6e72df86d1d5e1526
Rocco Wu <33598255+RoccoWu@users.noreply.github.com>
2023-04-30 16:30:54 -0700

    Initial Commit

---
+++ EnemyEncounterTool/src/NPCBehaviorComponent.java
@@ -0,0 +1,9 @@
+public class NPCBehaviorComponent {
+
+    public String name;
+    public int health;
+    public int walkSpeed;
+    public int runSpeed;
+    public int viewDistance;
+    public int blindedMovementSpeed;
+}
