1b2481d2c7dc26ff09ba84aa395efa8f0dcea396
Rocco Wu <wu.rocco@gmail.com>
2023-04-30 22:33:47 -0700

    EnemyType Enum

---
+++ EnemyEncounterTool/src/EnemyType.java
@@ -0,0 +1,32 @@
+public enum EnemyType {
+    THUGLEVEL1,
+    THUGLEVEL2,
+    THUGLEVEL3,
+    TANKTHUG,
+    AGILETHUG,
+    KNIFETHUG,
+    SHIELDTHUG,
+    BOSS;
+
+    public static String makeEnemyTypeMenu() {
+
+        String categoryMenu = "Choose a category to view items: ";
+        for(EnemyType t : EnemyType.values()){
+            categoryMenu += "\n" + (t.ordinal() + 1)
+                    + ": " + t.name();
+        }
+        return categoryMenu;
+
+    }
+    /**
+     *
+     * @param num (Indexed starting at 1)
+     * @return
+     */
+    public static EnemyType getEnemyNumber(int num) {
+        EnemyType[] allProducts = EnemyType.values();
+        return allProducts[num-1];
+    }
+}
+
+
