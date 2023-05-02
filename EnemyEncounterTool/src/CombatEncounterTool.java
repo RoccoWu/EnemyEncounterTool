0081a8fcd90ebbcdaf94af4de62d11e23cefbe69
Rocco Wu <33598255+RoccoWu@users.noreply.github.com>
2023-05-02 02:45:31 -0700

    Implementing createCombatEncounter in CombatEncounterTool

--- EnemyEncounterTool/src/CombatEncounterTool.java
+++ EnemyEncounterTool/src/CombatEncounterTool.java
@@ -38,7 +38,7 @@ public class CombatEncounterTool {
             {
                 userInput = bff.input("enter something to continue "); //user can choose from the menu
                 bff.print(userInput);
-                switch(userInput) //switch statement manages what the player can choose
+                switch(userInput) //switch statement manages what the user can choose
                 {
                     case "0":
                         createCombatEncounter();
@@ -59,18 +59,33 @@ public class CombatEncounterTool {
 
     public void createCombatEncounter()
     {
-        //TODO: create some shit here
+        //TODO: Create a combat encounter object, ask what the name for the combat encounter, ask for size of combat encounter, show list of enemyTypes to add, and add this combat encounter to a database of other combat encounters
+        String ceName = bff.input("What do you want to name this combat encounter?");
+        int ceSize = bff.inputInt("How many enemies will be in this combat encounter?");
+        for(int i = 0; i<= ceSize; i++)
+        {
+            //TODO: print enemy types, the user will select which enemy type, add that enemy type into a list of enemy types (npcs) that the Combat Encounter will hold, save the
+        }
+        boolean saveChoice = bff.inputYesNo("Do you want to save this combat encounter?");
+
+        if(saveChoice)
+        {
+            //add combat encounter to database of combat encounters
+        }
+
+        else {
+            //return to menu
+        }
     }
 
     public void viewAllCombatEncounter()
     {
-        //TODO: create some shit here
+        //TODO: View all of the combat encounters into a list and show their contents
     }
 
     public void lookUpEnemyTypes()
     {
-        //TODO: create some shit here
-
+        //TODO: list all the enemy types in a list use ENEMYTYPE Enum, when user inputs a number it will show info no the corresponding enemy type
     }
 
     public void quit()
